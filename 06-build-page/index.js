175e5032baeff43bfb21801d4b4d1dc3b04c6154
Ivan-rom <rom_1van@icloud.com>
2024-01-22 22:46:39 +0600

    feat: finish task 6 build page

---
+++ 06-build-page/index.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+
+const distPath = path.join(__dirname, 'project-dist');
+
+fs.mkdir(distPath, { recursive: true }, (err) => {
+  if (err) throw err;
+
+  /* creating style.css */
+  const stylePath = path.join(__dirname, 'styles');
+
+  const styleWriteStream = fs.createWriteStream(
+    path.join(distPath, 'style.css'),
+  );
+  fs.readdir(stylePath, (error, styleFiles) => {
+    if (error) return error;
+
+    for (const file of styleFiles) {
+      const readStream = fs.createReadStream(path.join(stylePath, file));
+      readStream.on('data', (chunk) => {
+        styleWriteStream.write(chunk);
+      });
+    }
+  });
+
+  /* copying assets */
+  const copyDir = path.join(distPath, 'assets');
+
+  fs.mkdir(copyDir, { recursive: true }, (err) => {
+    if (err) throw err;
+    fs.readdir(path.join(__dirname, 'assets'), (error, folders) => {
+      if (error) return error;
+      for (const folder of folders) {
+        const folderDir = path.join(copyDir, folder);
+        fs.mkdir(folderDir, { recursive: true }, (err) => {
+          if (err) throw err;
+          fs.readdir(path.join(__dirname, 'assets', folder), (error, files) => {
+            if (error) throw error;
+            for (const file of files) {
+              fs.copyFile(
+                path.join(__dirname, 'assets', folder, file),
+                path.join(copyDir, folder, file),
+                fs.constants.COPYFILE_FICLONE,
+                (err) => {
+                  if (err) throw err;
+                },
+              );
+            }
+          });
+        });
+      }
+    });
+  });
+
+  /* creating index.html */
+  const readHtmlStream = fs.createReadStream(
+    path.join(__dirname, 'template.html'),
+  );
+  readHtmlStream.on('data', (chunk) => {
+    let string = chunk;
+    fs.readdir(path.join(__dirname, 'components'), (err, files) => {
+      if (err) throw err;
+
+      for (const file of files) {
+        const readStream = fs.createReadStream(
+          path.join(__dirname, 'components', file),
+        );
+        readStream.on('data', (chunk) => {
+          string = string.toString().replace(/\{\{([^}]+)\}\}/g, (x) => {
+            const componentName = x.slice(2, -2);
+            if (componentName === path.parse(file).name) return chunk;
+            return x;
+          });
+          fs.writeFile(path.join(distPath, 'index.html'), string, () => {});
+        });
+      }
+    });
+  });
+});
